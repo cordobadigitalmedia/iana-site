00fd135fb7264bc9388a22d3ba937af1e0c2fc50
ismael <ismael@cordobadigitalmedia.com>
2024-11-16 22:01:02 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         >
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
